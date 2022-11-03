b286342cf89bf4da2797754c5058c85fbfe6228a
unknown <emanuelroblesavila@gmail.com>
2022-11-03 16:31:08 -0300

    Model

--- PI-Videogames-main/api/src/models/Genres.js
+++ PI-Videogames-main/api/src/models/Genres.js
@@ -3,48 +3,13 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey:true
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description:{
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    slug:{
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    released:{
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
-    background_image:{
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    rating:{
-      type: DataTypes.DECIMAL,
-      allowNull: true,
-    },
-    platforms: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
-      allowNull: false,
-    },
-    createdVideoGame: {
-      type:DataTypes.BOOLEAN,
-      allowNull:true,
-      defaultValue:true,
+  sequelize.define('genres', {
+
+    name:{
+        type:DataTypes.STRING,
+        allowNull: false
     }
-    
-  });
+    })
 };
 //                       _oo0oo_
 //                      o8888888o
