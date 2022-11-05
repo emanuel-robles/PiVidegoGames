52ddf1a4118411d735353eba3b2177eb2bac2107
unknown <emanuelroblesavila@gmail.com>
2022-11-05 15:20:20 -0300

    css

--- PI-Videogames-main/client/src/components/CreateGame.jsx
+++ PI-Videogames-main/client/src/components/CreateGame.jsx
@@ -2,28 +2,28 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { getGenres, postVideoGame } from "../redux/actions"
-import * as actions from "../redux/actions/index"
+import "../css/createGame.css"
 function validate(state){
   const error={}
 
   if(!state.name){
-    error.name='The name of the Videogame is required.'
+    error.name='Campo Requerido'
   }else if(state.name.length<5){
-    error.name='The length of the videogame name has to be more than 5'
+    error.name='Campo Requerido, Se nececita 5 carcacteres minimo'
   }
   if(!state.description){
-    error.description='the description is required'
+    error.description='Campo Requerido'
   }
   if(!state.rating){
-    error.rating='The rating is required'
+    error.rating='Campo Requerido'
   }else if(state.rating<0 || state.rating >5){
-    error.rating='The rating has to be a value beetwen 0 and 5'
+    error.rating='Campo Requerido el puntaje debe ser de 0 a 5'
   }
   if(state.genres.length===0){
-    error.genres='Should have at least 1 genre'
+    error.genres='Campo Requerido'
   }
   if(state.platforms.length===0){
-    error.platforms='Should select at least 1 platform'
+    error.platforms='Campo Requerido'
 
   }
 
@@ -31,7 +31,12 @@ function validate(state){
 }
 // Si no lo hacen asi los test no van a correr.
 const CreateMovie = (props) => {
+  
+  let plataformss = [
+    "PC","PlayStation","Xbox","Nintendo Switch","iOS", "Android","Nintendo","PS Vita","PSP","Wii","Game Boy","Atari","SEGA","PS5","PS4","PS3","PS2","PS1",
+  ];
   const dispatch = useDispatch();
+  const genres=useSelector(state=>state.genres)
   const [button,setButton]=useState(true)
   const [movie, setInput] = React.useState({
     name:"",
@@ -66,6 +71,26 @@ const CreateMovie = (props) => {
     if(Object.keys(error).length){
     setButton(false)}
   }
+  const handleSelectP=(e)=>{
+    e.preventDefault()
+    if(!movie.platforms.includes(e.target.value)){
+      setInput({
+        ...movie,
+        platforms:[...movie.platforms,e.target.value]
+      })}else{
+        console.log('Plataforma ya agregada.')
+      }
+  }
+  const handleSelect=(e)=>{
+    e.preventDefault()
+    if(!movie.genres.includes(e.target.value)){
+    setInput({
+      ...movie,
+      genres:[...movie.genres,e.target.value]
+    })}else{
+      console.log('genero ya agregado!')
+    }
+  }
   const handleOnSubmit=(e)=>{
     e.preventDefault()
     if(!movie.name||!movie.rating||!movie.description||!movie.platforms || !movie.genres){
@@ -84,77 +109,125 @@ const CreateMovie = (props) => {
     alert('Videogame created!')
   }
   return (
-    <div>
-      <form onSubmit={handleOnSubmit}>
-        <label name="name">Name: </label>
+    <div className='creat' >
+            <div>
+      <Link to='/home'><button className=''>Regresar Al Home</button></Link>
+      </div>
+      <form onSubmit={e=> handleOnSubmit(e)}>
+        <div className='cra1'>        
+          <label>Nombre: </label>
         <input
+            className={error?.name && 'danger'}
           type="text"
           name="name"
           value={movie.name}
-          onChange={handleInputChange}
+          onChange={e=> handleInputChange(e)}
         />
-        
-
-        <label name="name">Puntaje:</label>
+         <span className='error'>{error?.name ||''}</span>
+         </div>
+         <div className='cra2'>
+        <label>Puntaje:</label>
         <input
           type="number"
           name="rating"
           value={movie.rating}
-          onChange={handleInputChange}
+          onChange={e=> handleInputChange(e)}
         />
-
+          <span className='error'>{error?.rating ||''}</span>
+          </div>
+          <div  className='cra8'  >
         <label name="name">Description: </label>
         <textarea
           type="text"
           name="description"
           value={movie.description}
-          onChange={handleInputChange}
+          onChange={e=> handleInputChange (e)}
         />
-
+         <span className='error'>{error?.description ||''}</span>
+         </div>
+         <div className='cra3' >
         <label name="name">Slug: </label>
         <input
 
           type="text"
           name="slug"
           value={movie.slug}
-          onChange={handleInputChange}
+          onChange={e=> handleInputChange(e)}
         />
-         <label name="name">Fecha </label>
+        </div>
+        <div className='cra4' >
+         <label>Fecha </label>
         <input
 
           type="date"
           name="released"
           value={movie.released}
-          onChange={handleInputChange}
+          onChange={(e)=>handleInputChange(e)}
         />
+            <span className='error'>{error?.released ||''}</span>
+</div>
+<div className='cra5' >
          <label name="name">Image </label>
         <input
 
           type="text"
           name="background_image"
           value={movie.background_image}
-          onChange={handleInputChange}
+          onChange={(e)=>handleInputChange(e)}
         />
-           <label name="name">Generos </label>
+           <span className='error'>{error?.background_image ||''}</span>
+           </div>
+
+           <br/>
+<div className='cra6'>
+<label>Generos:</label>
+<select onChange={e=>handleSelect(e)}>
+              <option hidden key={0}>select...</option>
+            {genres.map(g=>{
+              return(<option  key={g.id} value={g.name}>{g.name}</option>
+            )})}
+            </select> 
+          <span className='error'>{error?.genres ||''}</span>
+
+</div>
+
+
+           {/* <label name="name">Generos </label>
+           
         <input
 
           type="text"
           name="Genres"
           value={movie.genres}
-          onChange={handleInputChange}
+          onChange={(e)=> handleInputChange(e)}
         />
-         <label name="name">Pataformas </label>
-        <input
+         <span className='error'>{error?.genres ||''}</span>
+ */}
 
-          type="text"
-          name="platforms"
-          value={movie.platforms}
-          onChange={handleInputChange}
-        />
-        
-        
+<div className='cra7'>
+<label name="name">Pataformas </label>
+       <select onChange={e=>handleSelectP(e)}>
+            <option hidden>select...</option>
+          {plataformss.map(p=>{
+            return(
+              
+              <option
+              
+              value={p}
+              name='platforms'
+              key={plataformss.indexOf(p)}
+              >{p}</option>
+              
+              )
+            })}
+            </select>
+            <span className='error'>{error?.plataformss ||''}</span>
 
-        <button type="submit">Create Movie</button>
+</div>
+
+  
+
+        <button type="submit">Create Game</button>
       </form>
 
     </div>
--- PI-Videogames-main/client/src/components/LadinPage.jsx
+++ PI-Videogames-main/client/src/components/LadinPage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import {Link} from "react-router-dom"
+import  "../css/ladinPage.css" 
 
 export default function LandigPage(){
     return(
-        <div>
-            <h1>Game Library</h1>
+        <div className="ladinPage" >
+            <h1 className="ladin"
+            
+            >Game Library</h1>
            <Link to="/home">
             <button>Home</button>
            </Link>
